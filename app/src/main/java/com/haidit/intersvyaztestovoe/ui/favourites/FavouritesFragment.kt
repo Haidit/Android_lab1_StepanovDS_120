fbb4f3a8f02622564aeb2f0788e23eaa51c27d67
stepx <stepxun2001@gmail.com>
2024-02-14 21:28:52 +0500

    RandomUtil commit

--- app/src/main/java/com/haidit/intersvyaztestovoe/ui/favourites/FavouritesFragment.kt
+++ app/src/main/java/com/haidit/intersvyaztestovoe/ui/favourites/FavouritesFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
@@ -147,5 +148,9 @@ class FavouritesFragment : Fragment() {
         val content = file.readText()
         val updatedContent = content.replace(oldLine, newLine)
         file.writeText(updatedContent)
+    }
+
+    private fun function1(text: String){
+        Toast.makeText(requireContext(), text, Toast.LENGTH_LONG).show()
     }
 }
