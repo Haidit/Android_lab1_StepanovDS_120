d7cda8609dbbc6215edbe589d9326c02019d5b8c
stepx <stepxun2001@gmail.com>
2024-02-14 21:24:01 +0500

    RandomUtil commit

--- app/src/main/java/com/haidit/intersvyaztestovoe/utils/RandomUtilForBranch1.kt
+++ app/src/main/java/com/haidit/intersvyaztestovoe/utils/RandomUtilForBranch1.kt
@@ -3,8 +3,6 @@ package com.haidit.intersvyaztestovoe.utils
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 
-fun Fragment.notRandomUtil(text: String) {
-    Toast.makeText(requireContext(), text, Toast.LENGTH_LONG).show()
+fun randomUtil(number: Int) = number.toString()
 
-}
 
